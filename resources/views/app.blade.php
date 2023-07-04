20880cdf9d4851c6d88923ec95b0e28c25adbcc5
Ryan.T <78358288+teffahiryan@users.noreply.github.com>
2023-07-04 09:34:34 +0200

    initialize douane express

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
